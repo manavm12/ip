43726cd0668a6838ae6d2518ce842cc9011b5d78
Manav Mehta <>
2025-01-31 17:18:19 +0800

    Change name of bot

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,15 @@
+public class Buddy {
+    public static void main(String[] args) {
+        String introduction = "Hello! I'm Buddy!";
+        String goodbye = "Bye. Hope to see you soon!";
+        String divider = "--------------------------";
+
+        System.out.println(divider);
+        System.out.println(introduction);
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println(goodbye);
+        System.out.println(divider);
+
+    }
+}
